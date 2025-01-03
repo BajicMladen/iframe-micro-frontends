f82239fb408428d4951d578ffaee605bcda0e653
BajicMladen <bajicmladen33@gmail.com>
2025-01-03 19:51:25 +0100

    Update formatting.

--- apps/container/src/App.jsx
+++ apps/container/src/App.jsx
@@ -1,15 +1,19 @@
-import Footer from './components/Footer/Footer.tsx'
-import Header from './components/Header/Header.tsx'
+import Footer from './components/Footer/Footer.tsx';
+import Header from './components/Header/Header.tsx';
 
 function App() {
-  return ( 
-   <div className='w-full h-screen overflow-y-scroll'>
-    <Header></Header>
-    <div className='w-full h-56 bg-green-100'> IFrame Vue.js</div>
-    <div className='w-full h-56 bg-blue-50'>IFrame Svelte.js</div>
-    <Footer></Footer>
-   </div>
-  )
+  return (
+    <div className='w-full h-screen overflow-y-scroll'>
+      <Header></Header>
+      <div className='w-full h-56 bg-green-100'> IFrame Vue.js</div>
+      <iframe
+        src='http://localhost:5174'
+        title='Book List'
+        style={{ border: 'none', width: '100%', height: '600px' }}
+      />
+      <Footer></Footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
