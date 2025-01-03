23f3b6019a7003f0bb6dfa9bd9dcbdd63675c0e0
BajicMladen <bajicmladen33@gmail.com>
2025-01-03 21:52:49 +0100

    Fix size of svelte IFrame

--- apps/container/src/App.jsx
+++ apps/container/src/App.jsx
@@ -5,11 +5,17 @@ function App() {
   return (
     <div className='w-full h-screen overflow-y-scroll'>
       <Header></Header>
-      <div className='w-full h-56 bg-green-100'> IFrame Vue.js</div>
+      {/* <div className='w-full h-56 bg-green-100'> IFrame Vue.js</div> */}
       <iframe
         src='http://localhost:5174'
         title='Book List'
-        style={{ border: 'none', width: '100%', height: '600px' }}
+        style={{
+          border: 'none',
+          width: '100%',
+          height: '140vh',
+          overflow: 'hidden',
+          scrolling: 'no',
+        }}
       />
       <Footer></Footer>
     </div>
